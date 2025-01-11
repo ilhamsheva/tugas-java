4c38661002152d6c0286e3932e1ed33b0d248e99
ilhamsheva <ilhamshv@gmail.com>
2025-01-11 21:58:45 +0700

    update

--- src/pis/LoginController.java
+++ src/pis/LoginController.java
@@ -17,7 +17,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class FXMLDocumentController {
+public class LoginController {
 
     @FXML
     private AnchorPane main_form;
