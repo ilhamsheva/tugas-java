c835e376c4c103ff68deaebdeef7a627af2b26b5
ilhamsheva <ilhamshv@gmail.com>
2025-01-12 11:47:44 +0700

    update

--- src/pis/PIS.java
+++ src/pis/PIS.java
@@ -12,7 +12,6 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import pis.Controller.*;
 
 /**
  *
@@ -22,11 +21,7 @@ public class PIS extends Application {
        
     @Override
     public void start(Stage stage) throws Exception {
-<<<<<<< HEAD
         Parent root = FXMLLoader.load(getClass().getResource("Design/Login.fxml"));
-=======
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
->>>>>>> 4c38661002152d6c0286e3932e1ed33b0d248e99
         
         Scene scene = new Scene(root);
            
